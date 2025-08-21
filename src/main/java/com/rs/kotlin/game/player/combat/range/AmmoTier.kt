5926debfa9e879f77e6bfc4544bf7d7b81fd7236
Raynna <andreas.henriksson96@gmail.com>
2025-08-21 07:00:37 +0200

    Bunch of fixes to dungeoneering Added bind & unbind for items within dungeoneering Update npcs to use the new combat data system Reworked some more npcs

--- src/main/java/com/rs/kotlin/game/player/combat/range/AmmoTier.kt
+++ src/main/java/com/rs/kotlin/game/player/combat/range/AmmoTier.kt
@@ -16,7 +16,22 @@ enum class AmmoTier(val priority: Int) {
     BLACK_BOLT(9),
     MITHRIL_BOLT(9),
     ADAMANT_BOLT(9),
-    RUNE_BOLT(9);
+    RUNE_BOLT(9),
+
+    /** Dungeoneering Arrowrs*/
+    NOVITE_ARROW(priority = 1),
+    BATHUS_ARROW(priority = 2),
+    MARMAROS_ARROW(priority = 3),
+    KRATONITE_ARROW(priority = 4),
+    FRACTITE_ARROW(priority = 5),
+    ZEPHYRIUM_ARROW(priority = 6),
+    ARGONITE_ARROW(priority = 7),
+    KATAGON_ARROW(priority = 8),
+    GORGONITE_ARROW(priority = 9),
+    PROMETHIUM_ARROW(priority = 10),
+    SAGITTARIAN_ARROW(priority = 11),
+
+    ;
 
     fun canUse(tier: AmmoTier): Boolean {
         return this.priority >= tier.priority
